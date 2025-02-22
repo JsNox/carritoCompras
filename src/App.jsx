dfbd1b714ff91c56477d3b699ba5957665d435b5
JsNox <joel.silos@hotmail.com>
2025-02-22 15:39:03 -0600

    componente product

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,40 @@
+import { useState } from "react"
 import { Header } from "./components/Header"
 import { Products } from "./components/Products"
-
-
+import { db } from "./data/db"
 
 
 function App() {
 
+    const [data, setData] = useState(db)
+    console.log(data)
 
-  return (
-    <>
-      <Header />
+    return (
+        <>
+            <Header />
 
-    <main className="container-xl mt-5">
-        <h2 className="text-center">Nuestra Colección</h2>
+            <main className="container-xl mt-5">
+                <h2 className="text-center">Nuestra Colección</h2>
 
-        <div className="row mt-5">
-            <Products />
-          
-            
-        </div>
-    </main>
+                <div className="row mt-5">
+                    {data.map((product) => (
+                        <Products
+                            key={ product.id } 
+                            product={ product }                                         
+                        />
+                    ))}
+
+                </div>
+            </main>
 
 
-    <footer className="bg-dark mt-5 py-5">
-        <div className="container-xl">
-            <p className="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
-        </div>
-    </footer>
-    </>
-  )
+            <footer className="bg-dark mt-5 py-5">
+                <div className="container-xl">
+                    <p className="text-white text-center fs-4 mt-4 m-md-0">Perfume - Todos los derechos Reservados</p>
+                </div>
+            </footer>
+        </>
+    )
 }
 
 export default App
